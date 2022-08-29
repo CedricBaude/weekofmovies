fcde0222ee7de53d5606a53649d966a773b326fb
CedricBaude <baude.cedric.perso@gmail.com>
2022-08-29 16:35:07 +0200

    manage form data

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const calendar = new VanillaCalendar({
     selector: "#myCalendar",
     onSelect: (data, elem) => {
         selectedDate = new Date(data.date).toISOString().split('T')[0];
-        alert(`Vous avez sélectionné le ${selectedDate}`);
+        //alert(`Vous avez sélectionné le ${selectedDate}`);
 
     },
     months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
@@ -10,3 +10,16 @@ const calendar = new VanillaCalendar({
 });
 
 let selectedDate = {};
+
+const movieForm = document.querySelector('#movie-form');
+movieForm.addEventListener('submit', addMovie);
+
+function addMovie(e) {
+    e.preventDefault();
+    const formData = new FormData(movieForm);
+    const title = formData.get('title');
+    const year = formData.get('year');
+    const duration = formData.get('duration');
+    const genre = formData.getAll('genre');
+    console.log('movie', { title, year, duration, genre, selectedDate });
+}
